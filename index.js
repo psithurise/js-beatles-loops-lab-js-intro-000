ffee40fdf6f1ad6d1f5b7556f902a6c8f35b17bc
Rose Stough <psithurise@programmer.net>
2019-03-20 23:37:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function johnLennonFacts(facts) {
   var i = 0
   var arr = []
   while(i < facts.length) {
-    arr.push(arr[i] + "!!!");
+    arr.push(`${facts[i]}!!!`);
     i++
   }
   return arr
